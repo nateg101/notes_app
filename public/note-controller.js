efb6f7bb00ee5c9f42704946a97d3fde43f6b36d
Nat Elliott-Green <natelliottgreen34@gmail.com>
2019-10-29 13:50:58 +0000

    Downloaded node, added a controller and app file which can access tags

---
+++ public/note-controller.js
@@ -0,0 +1,8 @@
+(function(exports){
+  function getAppDiv() {
+    document.getElementById('app').innerHTML = 'Jess made me type toilet :(';
+  }
+  exports.getAppDiv = getAppDiv
+})(this);
+
+getAppDiv();
--- spec/note-list-viewSpec.js
+++ spec/note-list-viewSpec.js
@@ -1,12 +1,12 @@
 (function(exports) {
-  function noteListView() {
-    var list = new noteListModel();
+  function testListView() {
+    let list = new noteListModel();
     list.addNote('Favourite food: pesto');
     list.addNote('Favourite drink: seltzer');
-    var note_view = new noteListView(list);
+    let note_view = new noteListView(list);
     assert.isTrue(note_view.displayHTML() === '<ul><li><div>Favourite food: pesto</div></li><li><div>Favourite drink: seltzer</div></li></ul>')
     console.log("noteListView passed.")
   }
   exports.noteListView = noteListView
-  noteListView();
+  testListView();
 })(this);
